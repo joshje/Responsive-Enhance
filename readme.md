313e966d9d9c560a96c957ecd15bac27f7c52486
Josh Emerson <josh.j.emerson@gmail.com>
2012-02-03 00:38:30 +0000

    Responsive enhance script created.

---
+++ readme.md
@@ -0,0 +1,9 @@
+Responsive Enhance
+==================
+
+This script will check whether an image is larger than a specified width and load a full resolution image if neccessary.
+
+    <img id="demo" src="http://dummyimage.com/400x300" alt="Responsive Image" data-fullsrc="http://dummyimage.com/800x600">
+    <script>responsiveEnhance(document.getElementById('demo'), 400);</script>
+    
+This will replace the image with the image defined in the `data-fullsrc` when the image is wider than 400 pixels.
---
+++ responsive-enhance.js
@@ -0,0 +1,27 @@
+var addEvent=function(){return document.addEventListener?function(a,c,d){if(a&&a.nodeName||a===window)a.addEventListener(c,d,!1);else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}:function(a,c,d){if(a&&a.nodeName||a===window)a.attachEvent("on"+c,function(){return d.call(a,window.event)});else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}}();
+
+var responsiveEnhance = function(img, width, monitor) {
+    console.log(img);
+    if (img.length) {
+        for (var i=0, len=img.length; i<len; i++) {
+            responsiveEnhance(img[i], width, monitor);
+        }
+    } else {
+        if (((' '+img.className+' ').replace(/[\n\t]/g, ' ').indexOf(' large ') == -1) && img.clientWidth > width) {
+            var fullimg = new Image();
+            addEvent(fullimg, 'load', function(e) {
+                img.src = this.src;
+                img.className += ' large';
+            });
+            fullimg.src = img.getAttribute('data-fullsrc');
+        }
+    }
+    if (monitor != false) {
+        addEvent(window, 'resize', function(e) {
+            responsiveEnhance(img, width, false);
+        });
+        addEvent(img, 'load', function(e) {
+            responsiveEnhance(img, width, false);
+        });
+    }
+};
