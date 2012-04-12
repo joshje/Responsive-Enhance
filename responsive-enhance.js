91a4df89131f0d31d77849617567c7d961f718ef
Josh Emerson <josh.j.emerson@gmail.com>
2012-04-13 00:02:15 +0100

    Removing a console log

--- responsive-enhance.js
+++ responsive-enhance.js
@@ -1,7 +1,6 @@
 var addEvent=function(){return document.addEventListener?function(a,c,d){if(a&&a.nodeName||a===window)a.addEventListener(c,d,!1);else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}:function(a,c,d){if(a&&a.nodeName||a===window)a.attachEvent("on"+c,function(){return d.call(a,window.event)});else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}}();
 
 var responsiveEnhance = function(img, width, monitor) {
-    console.log(img);
     if (img.length) {
         for (var i=0, len=img.length; i<len; i++) {
             responsiveEnhance(img[i], width, monitor);
